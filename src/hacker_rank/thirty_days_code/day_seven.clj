fbf1332c5f4ea028e38b8d3b5bcc11bb6c57ddad
Douglas Silva <dogusky.silva@outlook.com>
2021-12-06 18:25:21 -0300

    add solution day seven in 30 days of code

---
+++ src/hacker_rank/thirty_days_code/day_seven.clj
@@ -0,0 +1,4 @@
+(ns hacker-rank.thirty-days-code.day-seven)
+
+(defn solve [numbers]
+  (clojure.string/join " " (reverse numbers)))
--- test/hacker_rank/thirty_days_code.clj
+++ test/hacker_rank/thirty_days_code.clj
@@ -2,7 +2,9 @@
   (:require [clojure.test :refer :all]
             [hacker-rank.thirty-days-code.day-two :as day-two]
             [hacker-rank.thirty-days-code.day-three :as day-three]
-            [hacker-rank.thirty-days-code.day-six :as day-six]))
+            [hacker-rank.thirty-days-code.day-six :as day-six]
+            [hacker-rank.thirty-days-code.day-seven :as day-seven]
+            ))
 
 (deftest day-two-solve-test
   (testing "Zero parameters should be return 0"
@@ -26,3 +28,7 @@
     (is (= ["Duls oga"] (day-six/solve ["Douglas"])))
     (is (= ["abc def"] (day-six/solve  ["adbecf"])))
     (is (= ["Hce akr" "Rn ak"] (day-six/solve  ["Hacker" "Rank"])))))
+
+(deftest day-seven-solve-test
+  (testing "Outups of day seven should be valid"
+    (is (= "5 4 3 2 1" (day-seven/solve [1 2 3 4 5])))))
