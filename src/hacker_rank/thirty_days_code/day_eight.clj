e78272e593e1a059ef092500a3bcf331009dea07
Douglas Silva <dogusky.silva@outlook.com>
2021-12-12 21:03:25 -0300

    add solution day 8 in 30 days of code without tests

---
+++ src/hacker_rank/thirty_days_code/day_eight.clj
@@ -0,0 +1,22 @@
+(ns hacker-rank.thirty-days-code.day-eight)
+
+(def fptr (get (System/getenv) "OUTPUT_PATH"))
+
+(def n-contacts (Integer/parseInt (clojure.string/trim (read-line))))
+
+(def phone-book {})
+
+(doseq [_ (range n-contacts)]
+  (def contact (clojure.string/trim (read-line)))
+  (let [[contact-name contact-phone] (clojure.string/split contact #" ")]
+    (def phone-book (assoc phone-book contact-name contact-phone))
+    ))
+
+
+(doseq [_ (range n-contacts)]
+  (def search-name (clojure.string/trim (read-line)))
+  (if-let [phone-number (get phone-book search-name)]
+    (println (str search-name "=" phone-number))
+    (println  "Not found")
+    )
+  )
